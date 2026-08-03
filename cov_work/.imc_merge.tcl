a04333dd55391a3f2d43e669e046588f03ed1275
Kulkarni <akulkarni434@ece-linlabsrv01.ece.gatech.edu>
2026-08-03 16:35:05 -0400

    updated file log reports

---
+++ cov_work/.imc_merge.tcl
@@ -0,0 +1,2 @@
+merge -runfile cov_work/.run_list.txt -initial_model union_all -out merged.ucd -overwrite
+exit
---
+++ cov_work/.imc_report.tcl
@@ -0,0 +1,3 @@
+load -run cov_work/scope/merged.ucd
+report_metrics -summary -out cov_work/functional_summary -overwrite
+exit
