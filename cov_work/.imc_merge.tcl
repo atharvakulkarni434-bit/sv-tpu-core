b7422f504ffac1733e702f249a2ec0e58c401c6b
atharvakulkarni434-bit <atharva.kulkarni.434@gmail.com>
2026-09-10 17:42:08 -0400

    Delete cov_work directory

--- cov_work/.imc_merge.tcl
+++
@@ -1,2 +0,0 @@
-merge -runfile cov_work/.run_list.txt -initial_model union_all -out merged.ucd -overwrite
-exit
--- cov_work/.imc_report.tcl
+++
@@ -1,3 +0,0 @@
-load -run cov_work/scope/merged.ucd
-report_metrics -summary -out cov_work/functional_summary -overwrite
-exit
